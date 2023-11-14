a5f3cba1d0e4d18640b5d3b1372f4a4b38359155
Allen Buenaventura <115561893+allenbnvtra@users.noreply.github.com>
2023-11-14 21:16:26 +0800

    history

--- src/components/HistoryThead.jsx
+++ src/components/HistoryThead.jsx
@@ -7,10 +7,13 @@ const HistoryThead = () => {
         <th className="px-1 py-2 max-w-[2rem] font-light text-sm text-center sticky top-0 bg-slate-700 text-white sm:py-3 sm:font-medium lg:text-base">
           Billing period
         </th>
+        <th className="px-1 py-2 max-w-[2rem] font-light text-sm text-center sticky top-0 bg-slate-600 text-white sm:py-3 sm:font-medium lg:text-base">
+          Total monthly bill
+        </th>
         <th className="px-1 py-2 max-w-[2rem] font-light text-sm text-center sticky top-0 bg-slate-700 text-white sm:py-3 sm:font-medium lg:text-base">
           Status
         </th>
-        <th className="px-1 py-2 max-w-[2rem] font-light text-sm text-center sticky top-0 bg-slate-700 text-white sm:py-3 sm:font-medium lg:text-base">
+        <th className="px-1 py-2 max-w-[1rem] font-light text-sm text-center sticky top-0 bg-slate-600 text-white sm:py-3 sm:font-medium lg:text-base">
           Action
         </th>
       </tr>
